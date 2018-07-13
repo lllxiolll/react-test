f1afa658080cb14f0897c31e6d8ed66d2fe175aa
maura rodriguez <rodriguez.maura56@gmail.com>
2018-07-13 23:12:31 +0200

    list rendered

--- src/App.js
+++ src/App.js
@@ -2,39 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const URI = 'https://gist.githubusercontent.com/nept/0f311e330a7881fff35d9a8aca129bb2/raw/1227b03c6f85950095b302c4c0c5f5843a604094/cities.json';
+
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      isLoading : true,
-      data: [
-        {        }
-      ]
+      data: [],
     }
   }
 
   componentDidMount() {
-    var url = 'https://gist.githubusercontent.com/nept/0f311e330a7881fff35d9a8aca129bb2/raw/1227b03c6f85950095b302c4c0c5f5843a604094/cities.json';
-    fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data.map(user =>({
-      city: `${user.city}`,
-      growth_from_2000_to_2013: `${user.growth_from_2000_to_2013}`,
-      latitude: `${user.latitude}`,
-      longitude:`${user.longitude}`,
-      population: `${user.population}`,
-      rank:`${user.rank}`,
-      state: `${user.state}`
-    })))) 
-    .then(contacts => this.setState({
-      contacts,
-      isLoading: false
-    }))
-    .catch(error => console.log('Parsing failed', error))
+    fetch(URI)
+      .then(response => response.json())
+      .then(data => this.setState({
+        data
+      }))
+      .catch(error => console.log('Parsing failed', error))
   }
 
 
   render() {
+      const {data} = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -47,7 +36,16 @@ class App extends Component {
           <div className="icon"></div>
         </div>
         <div >
-          ##map
+          <ul>
+            {
+              data.map(
+                data =>
+                  <li key={data.key}>
+                    <a>{data.city}</a>
+                    <a>{data.rank}</a>
+                  </li>
+              )}
+          </ul>
         </div>
       </div>
     );
